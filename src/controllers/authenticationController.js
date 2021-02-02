214842342a7f8c6df86351143f42bc864828903c
Gabriel Pedro B Piazza <pedro.piazza99@gmail.com>
2021-02-02 19:53:20 -0300

    refactor: changed the error instance for user not found

--- src/controllers/authenticationController.js
+++ src/controllers/authenticationController.js
@@ -1,6 +1,5 @@
 const bcrypt = require('bcrypt');
 
-const NotFoundError = require('../errors/NotFoundError');
 const UnauthorizedError = require('../errors/UnauthorizedError');
 const User = require('../models/User');
 
@@ -8,7 +7,7 @@ class AuthController {
   async verifyUserEmailAndPassword({ email, password }) {
     const user = await User.findOne({ where: { email } });
 
-    if (!user) throw new NotFoundError();
+    if (!user) throw new UnauthorizedError();
 
     if (!bcrypt.compareSync(password, user.password)) throw new UnauthorizedError();
 
