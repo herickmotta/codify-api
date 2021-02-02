d7a9b9757ce602ba3bc5954d57051a0b6ca63112
thiagoribb <thiribeiro142@gmail.com>
2021-02-02 17:36:08 -0300

    refactor: usersController and signUpMiddleware

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-console */
 require('express-async-errors');
 require('dotenv').config();
 const express = require('express');
--- src/middlewares/signUpMiddleware.js
+++ src/middlewares/signUpMiddleware.js
@@ -1,3 +1,4 @@
+/* eslint-disable consistent-return */
 const User = require('../models/User');
 const userSchemas = require('../schemas/userSchemas');
 
