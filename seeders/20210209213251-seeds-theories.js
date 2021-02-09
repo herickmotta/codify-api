70859877aa45528ced16f21e7a6af551d6c76c2d
herickmotta <herickrossato@gmail.com>
2021-02-09 18:52:26 -0300

    feat: add chapters/topics/exercises seeds

---
+++ seeders/20210209213251-seeds-theories.js
@@ -0,0 +1,56 @@
+module.exports = {
+  up: async (queryInterface) => {
+    await queryInterface.bulkInsert('theories', [
+      {
+        topicId: 1,
+        youtubeLink: 'https://www.youtube.com/watch?v=Ptbk2af68e8',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        topicId: 2,
+        youtubeLink: 'https://www.youtube.com/watch?v=Ptbk2af68e8',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        topicId: 3,
+        youtubeLink: 'https://www.youtube.com/watch?v=Ptbk2af68e8',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        topicId: 4,
+        youtubeLink: 'https://www.youtube.com/watch?v=Ptbk2af68e8',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        topicId: 5,
+        youtubeLink: 'https://www.youtube.com/watch?v=Ptbk2af68e8',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        topicId: 6,
+        youtubeLink: 'https://www.youtube.com/watch?v=Ptbk2af68e8',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        topicId: 7,
+        youtubeLink: 'https://www.youtube.com/watch?v=Ptbk2af68e8',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        topicId: 8,
+        youtubeLink: 'https://www.youtube.com/watch?v=Ptbk2af68e8',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  down: async (queryInterface) => queryInterface.bulkDelete('theories', null, {}),
+};
