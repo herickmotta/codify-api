df44c6648b91c9cfa8f240b7f8bb892308454059
herickmotta <herickrossato@gmail.com>
2021-02-10 18:39:26 -0300

    feat: admin auth middleware

--- src/routers/adminRouter/index.js
+++ src/routers/adminRouter/index.js
@@ -7,41 +7,26 @@ const chaptersAdminRouter = require('./chaptersAdminRouter');
 const topicsAdminRouter = require('./topicsAdminRouter');
 const exercisesAdminRouter = require('./exercisesAdminRouter');
 const theoriesAdminRouter = require('./theoriesAdminRouter');
-const NotFoundError = require('../../errors/NotFoundError');
+const authAdminMiddleware = require('../../middlewares/authAdminMiddleware');
 
 router.post('/sign-in', (req, res) => {
   const { error } = adminSchemas.signIn.validate(req.body);
   if (error) return res.status(422).send({ error: error.details[0].message });
   const { username, password } = req.body;
   if (username === process.env.ADMIN_USERNAME && password === process.env.ADMIN_PASSWORD) {
-    const data = '';
     const secretKey = process.env.SECRET;
-    const token = jwt.sign(data, secretKey);
+    const token = jwt.sign('admin', secretKey, { expiresIn: '1h' });
     return res.send(token);
   }
   return res.sendStatus(409);
 });
 
-router.use('/', (req, res, next) => {
-  const authHeader = req.header('Authorization');
-  if (!authHeader) return res.sendStatus(401);
+router.post('/sign-out', (req, res) => res.sendStatus(200));
 
-  const token = authHeader.replace('JWT ', '');
-
-  if (!token) return res.status(401).json({ error: 'No token provided.' });
-  try {
-    jwt.verify(token, process.env.SECRET);
-    next();
-  } catch (execption) {
-    if (execption instanceof NotFoundError) return res.status(401).json({ error: 'Failed to authenticate token.' });
-    return res.status(500).json({ error: 'call the responsible person, routeError: /admin/sign-in ' });
-  }
-});
-
-router.use('/courses', coursesAdminRouter);
-router.use('/chapters', chaptersAdminRouter);
-router.use('/topics', topicsAdminRouter);
-router.use('/exercises', exercisesAdminRouter);
-router.use('/theories', theoriesAdminRouter);
+router.use('/courses', authAdminMiddleware, coursesAdminRouter);
+router.use('/chapters', authAdminMiddleware, chaptersAdminRouter);
+router.use('/topics', authAdminMiddleware, topicsAdminRouter);
+router.use('/exercises', authAdminMiddleware, exercisesAdminRouter);
+router.use('/theories', authAdminMiddleware, theoriesAdminRouter);
 
 module.exports = router;
