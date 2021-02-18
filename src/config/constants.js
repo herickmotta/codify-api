e66279d13f99aa9343c11d6ce60e6f7547d0e524
LNakandakare <lucasnakaan@gmail.com>
2021-02-18 18:33:33 -0300

    refactor: get curse data

--- src/config/constants.js
+++ src/config/constants.js
@@ -4,7 +4,7 @@ const DB_URL_DEV = process.env.DATABASE_URL_DEV;
 
 const DB_URL_PROD = process.env.DATABASE_URL;
 
-const PRODUCTION = true;
+const PRODUCTION = false;
 
 const DB_URL = PRODUCTION ? DB_URL_PROD : DB_URL_DEV;
 
